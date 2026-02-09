4164332a110d7f7ebff68824cca816201026a45e
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2026-02-09 18:23:47 +0700

    menambah QuizService untuk mengelola pembuatan dan pengambilan kuis harian

---
+++ app/Services/QuizService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Quiz;
+
+class QuizService
+{
+    public function createQuiz(array $data)
+    {
+        return Quiz::create($data);
+    }
+
+    public function getTodayQuizzes()
+    {
+        return Quiz::WhereDate('active_date', now())
+            ->inRandomOrder()
+            ->limit(5)
+            ->get();
+    }
+}
