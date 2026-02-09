8eff7400bc9b3ec60a4ba78a524254b987c89f63
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2026-02-10 03:56:45 +0700

    ubah timezone aplikasi menjadi Asia/Jakarta dan tambahkan route untuk login, pendaftaran siswa, dan pengiriman kuis

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\QuizController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\ResultController;
 
 Route::prefix('v1')->group(function () {
     Route::get('/quizzes', [QuizController::class, 'index']);
     Route::post('/quizzes', [QuizController::class, 'store']);
+    Route::post('/login', [StudentController::class, 'login']);
+    Route::post('/students', [StudentController::class, 'store']);
+    Route::post('/submit-quiz', [ResultController::class, 'submit']);
 });
