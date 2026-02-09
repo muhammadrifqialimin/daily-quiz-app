853eb66d612b92be4d00004005d1f7be44e0de55
muhammadrifqialimin <rifqi.alimin.ft23@mail.umy.ac.id>
2026-02-09 18:23:09 +0700

    menambah StoreQuizRequest untuk validasi input quiz

---
+++ app/Http/Requests/StoreQuizRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreQuizRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+    public function rules()
+    {
+        return [
+            'question' => 'required|string',
+            'option_a' => 'required|string',
+            'option_b' => 'required|string',
+            'option_c' => 'required|string',
+            'option_d' => 'required|string',
+            'answer' => 'required|in:A,B,C,D',
+            'active_date' => 'required|date',
+        ];
+    }
+}
